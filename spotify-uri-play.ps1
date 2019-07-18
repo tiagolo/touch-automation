8cc7b0039144e51b2d8c53b1f7d3fec164608752
Tiago Lopes da Costa <tiagolo@gmail.com>
2019-07-18 12:39:47 -0300

    Integration script for using on touch-portal: - Spotify play by uri via remote web api - Switch Windows default audio device by id

---
+++ spotify-uri-play.ps1
@@ -0,0 +1,13 @@
+﻿
+Param(
+  [String] $uri='spotify:playlist:4jkNfY9btZJoAiCO4vYs2U'
+)
+
+if (-Not (Get-Process Spotify -ErrorAction SilentlyContinue)) {
+  Spotify.exe
+  Start-Sleep 7
+}
+
+Import-Module .\ps-spotify\ps-spotify
+Start-Process $uri
+Set-SpotifyConnectPlay $uri
